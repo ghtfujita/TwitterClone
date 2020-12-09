96006e1fac0f04354a0002ddd4bc52b7c826bb38
takeshi fujita <mail.takeshi.fujita@gmail.com>
2020-12-09 11:22:52 +0900

    new view updated w/ strong params

--- app/controllers/twclones_controller.rb
+++ app/controllers/twclones_controller.rb
@@ -4,4 +4,12 @@ class TwclonesController < ApplicationController
   def new
     @twclone = Twclone.new
   end
+  def create
+    Twclone.create(twclone_params)
+    redirect_to new_twclone_path
+  end
+  private
+  def twclone_params
+    params.require(:twclone).permit(:content)
+  end
 end
