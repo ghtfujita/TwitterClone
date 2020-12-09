f5b58b94e7b5689f2893a6a0ae83865a7f3bc3b8
takeshi fujita <mail.takeshi.fujita@gmail.com>
2020-12-09 12:05:37 +0900

    edit functionality in place

--- app/controllers/twclones_controller.rb
+++ app/controllers/twclones_controller.rb
@@ -1,4 +1,5 @@
 class TwclonesController < ApplicationController
+  before_action :set_twclone, only: [:edit, :update]
   def index
     @twclones = Twclone.all
   end
@@ -13,8 +14,22 @@ class TwclonesController < ApplicationController
       render :new
     end
   end
+  def edit
+  end
+  def update
+    if @twclone.update(twclone_params)
+      redirect_to twclones_path, notice: "tweet edited!"
+    else
+      render :edit
+    end
+  end
+  def destroy
+  end
   private
   def twclone_params
     params.require(:twclone).permit(:content)
+  end
+  def set_twclone
+    @twclone = Twclone.find(params[:id])
   end
 end
