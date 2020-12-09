00c98c477b3c28057d98d2a7b938055503544f9c
takeshi fujita <mail.takeshi.fujita@gmail.com>
2020-12-09 11:49:19 +0900

    validation in place

--- app/controllers/twclones_controller.rb
+++ app/controllers/twclones_controller.rb
@@ -1,12 +1,17 @@
 class TwclonesController < ApplicationController
   def index
+    @twclones = Twclone.all
   end
   def new
     @twclone = Twclone.new
   end
   def create
-    Twclone.create(twclone_params)
-    redirect_to new_twclone_path
+    @twclone = Twclone.create(twclone_params)
+    if @twclone.save
+    redirect_to new_twclone_path, notice: "tweeted!"
+    else
+      render :new
+    end
   end
   private
   def twclone_params
